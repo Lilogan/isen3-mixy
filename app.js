748063d3258d6ed0d1dde0f7e39b704a387b1545
Lilogan <21369468+Lilogan@users.noreply.github.com>
2020-06-15 15:01:16 +0200

    Connection à au serveur MongoDB

--- app.js
+++ app.js
@@ -1,39 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv');
 
-// router setup
-var indexRouter = require('./routes/index');
+// Define environnement variables
+dotenv.config();
 
-var app = express();
+// Initiate Mongo Server
+const InitiateMongoServer = require('./config/db');
+InitiateMongoServer();
 
-// view engine setup
+// Router setup
+const indexRouter = require('./routes/index');
+
+// Initiate App
+const app = express();
+
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Define use
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// define route
+// Define route
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
