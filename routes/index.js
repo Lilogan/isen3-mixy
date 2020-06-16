79f789a6f76b5e7a343315c59c8db002d88cb3dd
Grobsety Benjamin <ben.marseille@live.fr>
2020-06-16 19:38:09 +0200

    page annexes

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,29 @@ router.get('/meteo', function (req, res, next) {
   res.render('meteo', { title: 'Méteo' });
 });
 
+router.get('/hotel', function (req, res, next) {
+  res.render('hotel', { title: 'Hotel' });
+});
+
+router.get('/vol', function (req, res, next) {
+  res.render('vol', { title: 'Vol' });
+});
+
+router.get('/restaurant', function (req, res, next) {
+  res.render('restaurant', { title: 'Restaurant' });
+});
+
+router.get('/bus', function (req, res, next) {
+  res.render('bus', { title: 'BUS' });
+});
+
+router.get('/metro', function (req, res, next) {
+  res.render('metro', { title: 'METRO' });
+});
+
+router.get('/taxi', function (req, res, next) {
+  res.render('taxi', { title: 'TAXI' });
+});
+
+
 module.exports = router;
