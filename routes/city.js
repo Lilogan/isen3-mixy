7fd384bec60ab765c7dae4c68fe5ad02f31223a4
Lilogan <21369468+Lilogan@users.noreply.github.com>
2020-06-22 19:39:00 +0200

    Modify Traduction

--- routes/city.js
+++ routes/city.js
@@ -32,7 +32,7 @@ router.get('/:city/activity', infoTrip.activity, (req, res) => {
   res.render('activite', { title: req.params.city, activities: req.activities });
 });
 
-router.get('/:city/trad', (req, res) => {
+router.get('/:city/traduction', (req, res) => {
   res.render('traduction', { title: req.params.city, trad: req.traduction });
 });
 
