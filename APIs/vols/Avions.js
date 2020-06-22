9976f344ac802bfc0b6dab6fb6e1916334d6ad87
Lilogan <21369468+Lilogan@users.noreply.github.com>
2020-06-22 10:51:11 +0200

    Add Avions

--- APIs/vols/Avions.js
+++ APIs/vols/Avions.js
@@ -1,33 +1,34 @@
+const url ="mongodb+srv://TeamMixy:NicoMixy@mixy-cu5lx.mongodb.net/test?retryWrites=true&w=majority"
+const MongoClient = require ('mongodb'). MongoClient;
+const dbName = 'dbTest';
 const axios= require('axios');
 
-async function getFlights() {
-  const data = await axios
-    .get('https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/calendar', {
-      headers : {
-        "x-access-token":"af470d59bdf253fcdb8b0f02b215a5ed",
-        "x-rapidapi-host":"travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
-        "x-rapidapi-key":"f93cf393a4msh3ea644a3bec766ap1ca0fejsn0a48d1bfef89",
-        useQueryString: true,
-      },
-      params: {
-        return_date:"2020-08",
-        currency:"EUR",
-        length:"10",
-        calendar_type:"departure_date",
-        destination:"BCN",
-        origin:"MOW",
-        depart_date:"2020-08"
-      },
+MongoClient.connect(url, function(err, client) {
+    const col = client.db(dbName).collection('Flights');
+    axios({
+    "method":"GET",
+    "url":"https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/calendar",
+    "headers":{
+    "content-type":"application/octet-stream",
+    "x-access-token":"af470d59bdf253fcdb8b0f02b215a5ed",
+    "x-rapidapi-host":"travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
+    "x-rapidapi-key":"f93cf393a4msh3ea644a3bec766ap1ca0fejsn0a48d1bfef89",
+    "useQueryString":true
+    },"params":{
+    "return_date":"2020-08",
+    "currency":"EUR",
+    "length":"10",
+    "calendar_type":"departure_date",
+    "destination":"BCN",
+    "origin":"MOW",
+    "depart_date":"2020-08"
+    }
     })
-    .then((res) => {
-      const data = res.data.data;
-      return data;
+    .then((response)=>{
+      console.log(response)
+      col.insert(response.data);
     })
-    .catch((error) => {
-      console.error(error);
-    });
-    console.log(data)
-    return data ;
-}
-
-console.log(getFlights());
+    .catch((error)=>{
+      console.log(error)
+    })
+})
